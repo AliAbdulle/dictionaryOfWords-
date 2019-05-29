aaf4db6fc9448fe23be27454656024345e2a17c0
AliAbdulle <alidhobey@gmail.com>
2019-05-28 22:36:46 -0500

    saved to finished the works

--- Program.cs
+++ Program.cs
@@ -35,6 +35,65 @@ namespace dictionaryOfWords
             {
                 Console.WriteLine($"The definition of {word.Key} is {word.Value}");
             }
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a few words
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+
+            // Add Dictionary to your `dictionaryOfWords` list
+            dictionaryOfWords.Add(excitedWord);
+
+
+            // create another Dictionary and add that to the list
+            Dictionary<string, string> mindWord = new Dictionary<string, string>();
+
+            mindWord.Add("word", "mindWord");
+            mindWord.Add("definition", "having, thinking, or characterized by a mindfull people, enthusiasm, eagerness");
+            mindWord.Add("part of speech", "adjective");
+            mindWord.Add("example sentence", "I am mindfull to learn C#!");
+
+            dictionaryOfWords.Add(mindWord);
+
+            /*
+                Iterate your list of dictionaries and output the data, You can use the two foreach() loops below to help you start your iteration.
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+            foreach (Dictionary<string, string> word in dictionaryOfWords)
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach (KeyValuePair<string, string> wordData in word)
+                {
+                    Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+                }
+            }
         }
     }
 }
